b39153bb2507b3a15ef071e63476a53a7b7dec09
jbshipman <jbshipman@icloud.com>
2020-03-16 12:45:54 -0500

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 
 // Write your code here
+
+const num1 = 2;
+const num2 = 31;
+let multiply = num1 * num2;
+
+let random = Math.floor(Math.random())+1;
+
+let mod = 14 % 5;
+
+let max = Math.max(2,3,5,20,17,11,19);
